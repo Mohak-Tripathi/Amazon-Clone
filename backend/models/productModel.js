875bff064860c49fdcf45a3fda49e892af7afd60
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-10-01 11:45:33 +0530

    ch12

---
+++ backend/models/productModel.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter product name'],
+        trim: true, //to remove space from starting and ending.
+        maxLength: [100, 'Product name cannot exceed 100 characters']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please enter product price'],
+        maxLength: [5, 'Product name cannot exceed 5 characters'],
+        default: 0.0
+    },
+    description: {
+        type: String,
+        required: [true, 'Please enter product description'],
+    },
+    ratings: {   // not individual rating but average rating by including all users rating.
+        type: Number,
+        default: 0
+    },
+    images: [  //array becz there can be multiple images of same product. 
+    //Also one single object contain two objects becz we are storing our images in cloudinary. And Cloudinary gives - "id" and "url" in response.
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            },
+        }
+    ],
+    category: {
+        type: String,
+        required: [true, 'Please select category for this product'],
+        enum: {
+            values: [
+                'Electronics',
+                'Cameras',
+                'Laptops',
+                'Accessories',
+                'Headphones',
+                'Food',
+                "Books",
+                'Clothes/Shoes',
+                'Beauty/Health',
+                'Sports',
+                'Outdoor',
+                'Home'
+            ],
+            message: 'Please select correct category for product'
+        }
+    },
+    seller: {
+        type: String,
+        required: [true, 'Please enter product seller']
+    },
+    stock: {
+        type: Number,
+        required: [true, 'Please enter product stock'],
+        maxLength: [5, 'Product name cannot exceed 5 characters'],
+        default: 0
+    },
+    numOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [ //array of objects
+        {
+            user: {
+                type: mongoose.Schema.ObjectId,
+                ref: 'User',
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {  //individual rating
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const app = require("./app.js");
+const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv")
 
 //Setting up Config files
 dotenv.config({path: "backend/config/config.env"})
 
+
+//Connecting to database
+connectDatabase()
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server has started om PORT ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
