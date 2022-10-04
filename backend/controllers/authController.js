e7b7d8b444959bc01eac10daa7976df2f8be1252
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-10-04 10:21:51 +0530

    user routes done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -275,3 +275,40 @@ exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
         user
     })
 })
+
+
+// Update user profile   =>   /api/v1/admin/user/:id
+exports.updateUserByAdmin = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role //admin can change the user role as well.
+    }
+
+    const user = await User.findByIdAndUpdate(req.params.id, newUserData, {   //req.params.id "remember"
+        new: true
+    })
+
+    res.status(200).json({
+        success: true
+    })
+})
+
+// Delete user by Admin  =>   /api/v1/admin/user/:id
+exports.deleteUserByAdmin = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return next(new ErrorHandler(`User does not found with id: ${req.params.id}`))
+    }
+
+    // // Remove avatar from cloudinary  => TODO
+    // const image_id = user.avatar.public_id;
+    // await cloudinary.v2.uploader.destroy(image_id);
+
+    await user.remove();
+
+    res.status(200).json({
+        success: true,
+    })
+})
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,42 +1,47 @@
-const express = require('express');
+const express = require("express");
 
-const router= express.Router(); 
+const router = express.Router();
 
-const {registerUser,
-     loginUser, 
-     logoutUser, 
-     forgotPassword,
-      resetPassword, 
-      getUserProfile, 
-      updatePassword,
-      updateProfile, 
-    allUsers,
-    getUserDetails} = require('../controllers/authController')
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  forgotPassword,
+  resetPassword,
+  getUserProfile,
+  updatePassword,
+  updateProfile,
+  allUsers,
+  getUserDetails,
+  updateUserByAdmin,
+  deleteUserByAdmin,
+} = require("../controllers/authController");
 
-const {isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
 
-router.route("/register").post(registerUser)
-router.route("/login").post(loginUser)
+router.route("/logout").get(isAuthenticatedUser, logoutUser); //logout is applicable to only those who have logged in first so "isAuthenticatedUser"
 
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
 
-router.route("/logout").get(isAuthenticatedUser, logoutUser)  //logout is applicable to only those who have logged in first so "isAuthenticatedUser"
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
 
-router.route("/password/forgot").post(forgotPassword)
-router.route("/password/reset/:token").put(resetPassword)
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
-router.route("/me").get(isAuthenticatedUser, getUserProfile)
-
-
-router.route("/password/update").put(isAuthenticatedUser, updatePassword)
-
-router.route("/me/update").put(isAuthenticatedUser, updateProfile)
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 
 //Admin Routes
 
-router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"),  allUsers)
-router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles("admin"),  getUserDetails)
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allUsers);
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserByAdmin)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUserByAdmin);
 
-
-
-module.exports = router
+module.exports = router;
